702b0c3dba13f310139ac3a093274decf14d11d1
nsaimk <nsaimkorkmaz1@gmail.com>
2025-05-05 21:55:33 +0100

    controller vs static/index

--- src/main/java/com/centrica/em/controller/PingController.java
+++ src/main/java/com/centrica/em/controller/PingController.java
@@ -6,10 +6,10 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class PingController {
 
-    // @GetMapping("/")
-    // public String root() {
-    //     return "Welcome to EM backend API!";
-    // }
+    @GetMapping("/")
+    public String root() {
+        return "Welcome to EM backend API!";
+    }
 
     @GetMapping("/api/ping")
     public String ping() {
